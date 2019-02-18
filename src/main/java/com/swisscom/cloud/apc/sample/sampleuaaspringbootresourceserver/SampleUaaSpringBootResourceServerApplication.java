fce27d0bf9d812bd3ee4860e56d16bb9c56b0ba8
Matthias Winzeler <matthias.winzeler@swisscom.com>
2019-02-18 17:00:10 +0100

    initalized from spring initializr

---
+++ src/main/java/com/swisscom/cloud/apc/sample/sampleuaaspringbootresourceserver/SampleUaaSpringBootResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.swisscom.cloud.apc.sample.sampleuaaspringbootresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleUaaSpringBootResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleUaaSpringBootResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swisscom/cloud/apc/sample/sampleuaaspringbootresourceserver/SampleUaaSpringBootResourceServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swisscom.cloud.apc.sample.sampleuaaspringbootresourceserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleUaaSpringBootResourceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
